5b763187b92b52269ee56978801dc587484206a0
Chris Taylor <crntaylor@gmail.com>
2012-10-16 14:32:51 +0100

    Master function - train classifiers, write output file

---
+++ master.m
@@ -0,0 +1,30 @@
+function master()
+%MASTER Master function. Reads in data, processes it, runs learning algorithms to classify the test
+%cases and outputs the result to a file.
+
+    % Load the training/test data
+
+    [y xtrain xtest] = loadData;
+
+    % Plot sample to check
+
+    plotData(xtrain);
+    title('Training data (sample)')
+
+    % Do PCA reduction
+
+    [wtrain coeff] = pcaReduce(xtrain,0.9); % Keep 90% of the variance
+
+    wtest = xtest * coeff;
+
+    % KNN classifier
+
+    k = 1;
+
+    pred = knn(y,wtrain,wtest,k); % This takes about 10-15 minutes
+
+    % Write output to disk
+
+    csvwrite('Output/predictions.csv',pred);
+
+end
