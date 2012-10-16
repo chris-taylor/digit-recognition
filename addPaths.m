99a5b4ccdd69c40f3d548a9fd303d57ed9e576cf
Chris Taylor <crntaylor@gmail.com>
2012-10-16 16:17:24 +0100

    Add nn paths

--- addPaths.m
+++ addPaths.m
@@ -1,3 +1,7 @@
+restoredefaultpath
 addpath(pwd)
+addpath(fullfile(pwd,'NN'))
 addpath(fullfile(pwd,'KNN'))
 addpath(fullfile(pwd,'Util'))
+addpath(fullfile('../..','DeepLearnToolbox/NN'))
+addpath(fullfile('../..','DeepLearnToolbox/Util'))
