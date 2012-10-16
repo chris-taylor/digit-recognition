c15a084366ff187acb0c9f715c89aca01cec61f5
Chris Taylor <crntaylor@gmail.com>
2012-10-16 01:13:30 +0100

    Loop over all training data

--- KNN/knnTest.m
+++ KNN/knnTest.m
@@ -1,23 +1,8 @@
-function mcr = knnTest
+function [k m] = knnTest
 
     [ytrain xtrain] = loadData;
     
-    trainidx = randi(42000,1000,1);
-    testidx = randi(42000,1000,1);
-    
-    x1 = xtrain(trainidx,:);
-    x2 = xtrain(testidx,:);
-    
-    y1 = ytrain(trainidx,:);
-    y2 = ytrain(testidx,:);
-    
-    k = 10;
-    
-    pred = knn(y1,x1,x2,k);
-    
-    mcr = mean(pred ~= y2);
-    
-    [k m] = knnCrossval(y1,x1);
+    [k m] = knnCrossval(ytrain,xtrain);
     
     plot(k,m)
 
